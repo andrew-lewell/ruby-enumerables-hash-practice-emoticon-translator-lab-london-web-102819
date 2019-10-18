fa2b15b787cb06e1604efd3f41c3cd701bdfe358
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-18 10:27:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,6 @@ end
 
 def get_english_meaning(file_path, emoticon)
   lib = load_library(file_path)
-  japenese = lib[:get_emoticon][emoticon]
+  japanese = lib[:get_emoticon][emoticon]
   meaning = lib[:get_meaning][japanese]
 end
